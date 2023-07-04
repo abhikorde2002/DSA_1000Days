c25b510f5676f43978f18e66991c1a3c75033e27
Abhishek korde <abhishekkorde44@gmail.com>
2023-07-04 17:55:01 +0530

    Sorting code

--- Array/Day2/Missing_Number.java
+++ Array/Day2/Missing_Number.java
@@ -4,6 +4,7 @@ public class Missing_Number {
     public static void main(String[] args) {
         int arr[]={0,1,3};
         Missing(arr);
+        missing(arr);
     }
 
     static void Missing(int arr[]){
@@ -13,6 +14,16 @@ public class Missing_Number {
             sum+=i;
         }
         System.out.println((size * (size+1))/2 -sum);
+    }
+
+    static void missing(int arr[]){
+        int count=0;
+        for(int i=0;i<arr.length;i++){
+            if(count!= arr[i]){
+               System.out.println(count);
+            }
+            count++;
+        }
     }
     }
 
