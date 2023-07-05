9d4c5e1267b1c29eaf7b873c803dd06b6a6120a8
Abhishek korde <abhishekkorde44@gmail.com>
2023-07-05 11:29:22 +0530

    Kth Smallest

---
+++ Array/Day4/KthSmallest.java
@@ -0,0 +1,26 @@
+package Array.Day4;
+
+import java.util.PriorityQueue;
+
+public class KthSmallest {
+    public static void main(String[] args) {
+        int num[]={10,20,30,40,50,60};
+        kthsmallest(num, 3);
+    }
+
+    static void kthsmallest(int num[],int k){
+        PriorityQueue<Integer> pQueue= new PriorityQueue<>(); 
+
+        for(int i=0;i<k;i++){
+            pQueue.add(num[i]);
+        }
+
+        for(int i=k;i<num.length;i++){
+              if(pQueue.peek() < num[i]){
+                pQueue.poll();
+                pQueue.add(num[i]);
+              }
+        }
+        System.out.println(pQueue.peek());
+    }
+}
