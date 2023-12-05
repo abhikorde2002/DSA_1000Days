2157be3e194860c223af053c63ffb9a79c358bce
Abhishek korde <abhishekkorde44@gmail.com>
2023-12-05 11:23:38 +0530

    434. Number of Segments in a String

--- Strings/Day_43/Num_seg.java
+++ Strings/Day_43/Num_seg.java
@@ -1,9 +1,12 @@
 package Strings.Day_43;
 
+import java.util.StringTokenizer;
+
 public class Num_seg {
     public static void main(String[] args) {
         String s=", , , ,        a, eaefa";
         System.out.println(countSegment(s));
+        System.out.println(token(s));
     }
 
     static int countSegment(String str){
@@ -19,5 +22,15 @@ public class Num_seg {
             }
         }
         return count;
+    }
+
+    static int token(String s){
+        StringTokenizer str= new StringTokenizer(s," ");
+        int cou=0;
+        while (str.hasMoreTokens()) {
+            cou++;
+            str.nextToken();
+        }
+        return cou;
     }
 }
