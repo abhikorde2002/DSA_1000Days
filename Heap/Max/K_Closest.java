3006c71b2db67943448e1d3572c777bdb69cd289
Abhishek Korde <abhishekkorde44@gmail.com>
2024-05-31 23:25:06 +0530

    code added

---
+++ Heap/Max/K_Closest.java
@@ -0,0 +1,13 @@
+package Heap.Max;
+
+import java.util.PriorityQueue;
+
+public class K_Closest {
+    public static void main(String[] args) {
+        
+    }
+
+    static int[][] kClosest(int[][] arr, int k) {
+        PriorityQueue<Integer , <Integer,Integer>> map= new PriorityQueue<>();
+    }
+}
